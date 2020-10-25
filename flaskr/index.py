41b6177080de72eca6f14e8a36435099e5c892ab
licykk <licykk@gmail.com>
2020-10-24 23:29:37 -0400

    login works

---
+++ flaskr/index.py
@@ -0,0 +1,15 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, Flask, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('index', __name__)
+
+@bp.route('/')
+def index():
+
+    return render_template('index.html')
